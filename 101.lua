dde986e53aac1eaaffc0ba3ec1b66e0f81afeb9a
Tim Menzies <timm@ieee.org>
2022-10-15 19:15:48 -0400

    save

--- 101.lua
+++ 101.lua
@@ -1,9 +1,9 @@
 -- ## Example, CLI and test engines.   
 -- (c)2022, Tim Menzies <timm@ieee.org> BSD2 license.        
--- Yet another demo of "less is more".     
---     
--- > This code is inspired by DocOpt (but tries to be ten times shorter).
--- Parses help text to make table of settings+defaults (see example in 101.lua).
+-- Yet another demo of "less is more", this code is inspired by DocOpt 
+-- (but tries to be ten times shorter).
+--    
+-- > Parses help text to make table of settings+defaults (see example in 101.lua).
 -- Updates those settings from command-line args.
 -- Supports a simple test suite (see example in test/eg.lua).
 -- One or all test suite items can be called from the command line.
